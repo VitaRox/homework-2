a33bc7c73f70d6127650ea94df0a85e2051a3060
Vita <vita.harvey@seattlecolleges.edu>
2022-03-07 13:29:45 -0800

    - Having found where I wrote HW #2, I completed it;
    vita.aubergine@gmail.com

--- homeworkTwo.js
+++ homeworkTwo.js
@@ -35,8 +35,74 @@ an element
 
 */
 
-/*
-- Homework: without using filter function from array, write a function that will filter out a list of strings that are palindrome from the array. Try not to use online examples to know how to find if a string is palindrome.
+// Helper function; it made sense to me to extract this logic into a separate, recursive function
+// for determining whether a given string is a palindrome
+function isPalindrome(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - 1]) {
+      return false;
+    } else {
+      str = str.slice(i + 1, str.length - 1);
+      return isPalindrome(str);
+    }
+  }
+  return true;
+}
 
-- Homework: write a function that helps me find out how many unique numbers are in an array. Unique numbers can at most only have one occurrence in the array.(Non-duplicate numbers)
+// Test cases:
+
+// console.log(isPalindrome('racecar')); // should be true
+// console.log(isPalindrome('madam'));  // should be true
+// console.log(isPalindrome('uniform'));  // should be false
+// console.log(isPalindrome('scabs'));  // should be false
+// console.log(isPalindrome('deified'));  // should be true
+// console.log(isPalindrome('refer'));  // should be true
+// console.log(isPalindrome('spoon'));  // should be false
+// console.log(isPalindrome('crumbs'));  // should be false
+console.log('It works!');
+
+/*
+  Part II
+  - Write a function that will filter out a list of strings that are palindrome from the array.
+  Try not to use online examples to know how to find if a string is palindrome.
 */
+function filterNonPalindromes(filterArray) {
+  let finalArray = [];
+  for (let item of filterArray) {
+    if (isPalindrome(item)) {
+      finalArray.push(item);
+    }
+  }
+  console.log(`The final, filtered array consists of ${finalArray.toString()}`);
+  let msg = "That's all, folks!";
+  return msg;
+}
+
+
+let stringTestArray = ['racecar', 'zamboni', 'madam', 'madamoiselle', 'monsieur', 'refer', 'deified'];
+console.log(filterNonPalindromes(stringTestArray));
+
+
+/*
+  Part II
+- Homework: write a function that helps me find out how many unique numbers are in an array.
+  Unique numbers can at most only have one occurrence in the array.(Non-duplicate numbers)
+*/
+function countUniqueNumbers(inArray) {
+  let count = 0;
+  for (let num of inArray) {
+    if (inArray.indexOf(num) === inArray.lastIndexOf(num)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// Test cases
+let numTestArray1 = [2, 2, 5, 4, 56, 87, 9999, 32, 1, 4, 5, 6, 7];
+console.log(`There are ${countUniqueNumbers(numTestArray1)} unique values in our test array`);
+console.log(`There should be 7 unique values in our test array.`)
+
+let numTestArray2 = [1, 1, 1, 4, 55, 100, 1];
+console.log(`There are ${countUniqueNumbers(numTestArray2)} unique values in our test array`);
+console.log(`There should be 3 unique values in our test array.`);
